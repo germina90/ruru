bc7a2c818ff1990cc3d061dd1aef8b888833e1de
Tom Hanks <germina90@gmail.com>
2018-11-12 18:39:49 -0500

    adding config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Matrix8%d');
 define('DB_NAME', 'mysql');
  
 /* Attempt to connect to MySQL database */
